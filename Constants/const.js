37c8acef6f8d394c47d1920d7b8b8adfe194b218
EduardoAlbert <albert-costa1@live.com>
2019-10-09 10:38:49 -0300

    constants

---
+++ Constants/const.js
@@ -0,0 +1,16 @@
+/* const anoNascimento = 1992;
+console.log(anoNascimento)
+
+if (true) {
+  const autor = 'Edu';
+}
+console.log(autor)
+
+const nome = 'Fulano';
+
+nome = 'Edu' */
+
+const competencias = ['JavaScript', 'Python', 'HTML5'];
+
+competencias[1] = 'CSS';
+console.log(competencias)
