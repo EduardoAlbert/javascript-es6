e6100ef832a1656ce5d965de108954949c380b3a
EduardoAlbert <albert-costa1@live.com>
2019-10-09 13:08:21 -0300

    Hoisting

---
+++ Hoisting/hoisting-functions.js
@@ -0,0 +1,7 @@
+console.log(soma(1, 1))
+
+// ^^^^^^^^^^^^^ //
+
+function soma(a, b) {
+  return a + b
+}
---
+++ Hoisting/hoisting.js
@@ -0,0 +1,15 @@
+/* var a
+console.log(a);
+a = 123;
+console.log(a);
+ */
+
+/* console.log(ddd);
+let ddd = 21; */
+
+
+console.log(soma(1, 1))
+
+var soma = function(a, b) {
+  return a + b
+}
