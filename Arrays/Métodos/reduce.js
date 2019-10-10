6b895b8362b226b0d58b73f16a94f13508850e68
EduardoAlbert <albert-costa1@live.com>
2019-10-10 16:22:06 -0300

    reduce

--- Arrays/Métodos/reduce.js
+++ Arrays/Métodos/reduce.js
@@ -1,6 +1,6 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-somaArray = array.reduce(function(primeiro, segundo) {
+somaArray = array.reduce(function(valorAcumulador, valorArray) {
     return primeiro + segundo;
 });
 
